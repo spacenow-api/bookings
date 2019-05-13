898b4c16e515cf1df69588a72cecbb009a3952a8
lucasSpacenow <csalucasnascimento@gmail.com>
2019-05-13 16:58:34 +1000

    adding stage

---
+++ core/cancel.js
@@ -0,0 +1,32 @@
+import uuid from "uuid"
+
+import * as dynamoDbLib from "../libs/dynamodb-lib"
+import { success, failure } from "../libs/response-lib"
+
+export async function main(event, context) {
+
+  const data = JSON.parse(event.body)
+
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      listingId: event.requestContext.listingId,
+      bookingId: event.pathParameters.bookingId
+    },
+    UpdateExpression: "SET bookingState = :bookingState, updatedAt = :updatedAt",
+    ExpressionAttributeValues: {
+      ":bookingState": "Cancelled",
+      ":updatedAt": Date.now() || null
+    },
+    ReturnValues: "ALL_NEW"
+  }
+
+  try {
+    await dynamoDbLib.call("update", params)
+    return success({ status: true })
+  } catch (e) {
+    console.log(e)
+    return failure({ status: false })
+  }
+
+}
