d119a6fc25e58732d28b26f6b6057d5c1d4ee1e8
lucasSpacenow <csalucasnascimento@gmail.com>
2019-05-14 12:36:30 +1000

    fix: adding new call

--- core/getByHostId.js
+++ core/getByHostId.js
@@ -5,13 +5,16 @@ export const main = async (event, context) => {
   
   const params = {
     TableName: process.env.tableName,
-    Key: {
-      hostId: event.hostId
+    IndexName: "bookingId-index",
+    ProjectionExpression: "hostId",
+    FilterExpression: "hostId = :hostId",
+    ExpressionAttributeValues: {
+        ":hostId": event.hostId
     }
   }
 
   try {
-    const result = await dynamoDbLib.call("get", params);
+    const result = await dynamoDbLib.call("scan", params);
     if (result.Item)
       return success(result.Item)
     else
