55b800dcd563bed7d8e9d1dbfb5f9eed5b5d62f8
lucasSpacenow <csalucasnascimento@gmail.com>
2019-05-14 11:53:06 +1000

    fix: changing file name

---
+++ core/getByHostId.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from "../libs/dynamodb-lib"
+import { success, failure } from "../libs/response-lib"
+
+export const main = async (event, context) => {
+  
+  const params = {
+    TableName: process.env.tableName,
+    IndexName: "bookingId-index",
+    ProjectionExpression: "hostId",
+    KeyConditionExpression: "hostId = :hostId",
+    ExpressionAttributeValues: {
+        ":hostId": event.hostId
+    }
+  }
+
+  try {
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item)
+      return success(result.Item)
+    else
+      return failure({ status: false, error: "Booking not found." })
+  } catch (e) {
+    return failure({ status: false })
+  }
+
+}
