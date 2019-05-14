b81efdb84bbb6e786cf094ed9651b050ced07db6
lucasSpacenow <csalucasnascimento@gmail.com>
2019-05-14 11:13:07 +1000

    fix: changing file name

--- core/list.js
+++ core/list.js
@@ -4,11 +4,7 @@ import { success, failure } from "../libs/response-lib"
 export const main = async (event, context) => {
   
   const params = {
-    TableName: process.env.tableName,
-    KeyConditionExpression: "listingId = :listingId",
-    ExpressionAttributeValues: {
-      ":listingId": event.listingId,
-    }
+    TableName: process.env.tableName
   }
 
   try {
