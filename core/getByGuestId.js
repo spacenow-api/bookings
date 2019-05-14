5b8e8ecbd31a34ca8e14501e2d7102c03cf71af8
lucasSpacenow <csalucasnascimento@gmail.com>
2019-05-14 12:56:36 +1000

    fix: adding query by host id

---
+++ core/getByGuestId.js
@@ -0,0 +1,25 @@
+import * as dynamoDbLib from "../libs/dynamodb-lib"
+import { success, failure } from "../libs/response-lib"
+
+export const main = async (event, context) => {
+  
+  const params = {
+    TableName: process.env.tableName,
+    FilterExpression: "#guestId = :guestId",
+    ExpressionAttributeNames:{
+        "#guestId": "guestId"
+    },
+    ExpressionAttributeValues: {
+      ":guestId": event.guestId
+    }
+  }
+
+  try {
+    const result = await dynamoDbLib.call("scan", params);
+    return success(result.Items)
+  } catch (e) {
+    console.log(e)
+    return failure({ status: false })
+  }
+
+}
