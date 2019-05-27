acf5c0146c7783d8d3b7536d1d7cb5f0ac6f8e1e
lucasSpacenow <csalucasnascimento@gmail.com>
2019-05-27 10:52:41 +1000

    fix: removing body

--- core/create.js
+++ core/create.js
@@ -6,8 +6,7 @@ import { success, failure } from "../libs/response-lib"
 
 export const main = async (event, context) => {
 
-  console.log("EVENT ===>>> ", event);
-  const data = JSON.parse(event)
+  const data = JSON.parse(event.body)
   const queueUrl = `https://sqs.${process.env.region}.amazonaws.com/${process.env.accountId}/${process.env.queueName}`;
   const bookingId = uuid.v1();
 
