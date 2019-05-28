745145d872cad0a3119cfbd30931b14111fac55b
lucasSpacenow <csalucasnascimento@gmail.com>
2019-05-28 17:03:29 +1000

    validations

--- core/create.js
+++ core/create.js
@@ -1,66 +1,62 @@
-import uuid from 'uuid';
+import uuid from "uuid"
 
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import * as queueLib from '../libs/queue-lib';
-import { success, failure } from '../libs/response-lib';
+import * as dynamoDbLib from "../libs/dynamodb-lib"
+import * as queueLib from "../libs/queue-lib"
+import { success, failure } from "../libs/response-lib"
+import { calcTotal, getDates } from '../validations'
 
-export const main = async event => {
-  const queueUrl = `https://sqs.${process.env.region}.amazonaws.com/${
-    process.env.accountId
-  }/${process.env.queueName}`;
+export const main = async (event, context) => {
 
-  const data = JSON.parse(event.body);
-
+  const data = JSON.parse(event.body)
+  const queueUrl = `https://sqs.${process.env.region}.amazonaws.com/${process.env.accountId}/${process.env.queueName}`;
   const bookingId = uuid.v1();
 
   const confirmationCode = Math.floor((100000 + Math.random()) * 900000);
-
-  let hostServiceFee = 0;
-  let guestServiceFee = 13.5;
-  if (data.isAbsorvedFee) {
-    hostServiceFee = 10;
-    guestServiceFee = 3.5;
-  }
+  const guestServiceFee = data.isAbsorvedFee ? 1.035 : 1.135;
+  const hostServiceFee = data.isAbsorvedFee ? 1.1 : 1;
 
   const params = {
     TableName: process.env.tableName,
     Item: {
-      bookingId: bookingId,
       listingId: data.listingId,
-      confirmationCode: confirmationCode,
+      bookingId: bookingId,
       hostId: data.hostId,
       guestId: data.guestId,
-      quantity: 1,
+      reservations: data.reservations,
+      quantity: data.quantity || 1,
       basePrice: data.basePrice,
+      fees: data.fees,
+      period: data.period,
       currency: data.currency,
       guestServiceFee: guestServiceFee,
       hostServiceFee: hostServiceFee,
-      totalPrice: data.totalPrice,
-      paymentState: 'pending',
-      bookingState: 'pending',
+      totalPrice: calcTotal(data.basePrice, guestServiceFee, data.reservations),
+      confirmationCode: confirmationCode,
+      paymentState: "pending",
+      payoutId: data.payoutId,
+      bookingState: "pending",
       bookingType: data.bookingType,
+      paymentMethodId: data.paymentMethodId,
+      subscriptionId: data.subscriptionId,
+      sourceId: data.sourceId,
       priceType: data.priceType,
-      periodQuantity: data.periodQuantity,
-      reservations: data.reservations,
       updatedAt: Date.now(),
       createdAt: Date.now()
     }
-  };
+  }
 
   const paramsQueue = {
     QueueUrl: queueUrl,
-    MessageBody: JSON.stringify({
-      bookingId: bookingId,
-      listingId: data.listingId,
-      blockedDates: data.reservations
-    })
-  };
+    MessageBody: JSON.stringify({ bookingId: bookingId, listingId: data.listingId, blockedDates: data.reservations })
+  }
 
   try {
-    await dynamoDbLib.call('put', params);
+    await dynamoDbLib.call("put", params);
     await queueLib.call(paramsQueue);
     return success(params.Item);
   } catch (e) {
-    return failure({ status: 'error', error: e });
+    console.log(e)
+    return failure({ status: false })
   }
-};
+  
+}
---
+++ validations/index.js
@@ -0,0 +1,27 @@
+const calcTotal = (bookingType, basePrice, quantity, days, serviceFee) => {
+
+  switch(bookingType) {
+    case 'hourly':
+
+      break;
+    case 'daily':
+      break;
+    case 'weekly':
+      break;
+    case 'monthly':
+      break;
+  }
+
+}
+
+const getDates = (startDate, period, bookingType) => {
+  switch(bookingType) {
+    case 'weekly':
+
+      break;
+    case 'monthly':
+      break;
+  }
+}
+
+export { calcTotal, getDates };
