b85c0fd240a88ae88b035e47ff7b23fa9d6d0501
Arthemus <arthemus.moreira@gmail.com>
2019-05-28 14:57:11 +1000

    Refine fields and create logic.

--- core/create.js
+++ core/create.js
@@ -1,56 +1,66 @@
-import uuid from "uuid"
+import uuid from 'uuid';
 
-import * as dynamoDbLib from "../libs/dynamodb-lib"
-import * as queueLib from "../libs/queue-lib"
-import { success, failure } from "../libs/response-lib"
+import * as dynamoDbLib from '../libs/dynamodb-lib';
+import * as queueLib from '../libs/queue-lib';
+import { success, failure } from '../libs/response-lib';
 
-export const main = async (event, context) => {
+export const main = async event => {
+  const queueUrl = `https://sqs.${process.env.region}.amazonaws.com/${
+    process.env.accountId
+  }/${process.env.queueName}`;
 
-  const data = JSON.parse(event.body)
-  const queueUrl = `https://sqs.${process.env.region}.amazonaws.com/${process.env.accountId}/${process.env.queueName}`;
+  const data = JSON.parse(event.body);
+
   const bookingId = uuid.v1();
+
+  const confirmationCode = Math.floor((100000 + Math.random()) * 900000);
+
+  let hostServiceFee = 0;
+  let guestServiceFee = 13.5;
+  if (data.isAbsorvedFee) {
+    hostServiceFee = 10;
+    guestServiceFee = 3.5;
+  }
 
   const params = {
     TableName: process.env.tableName,
     Item: {
-      listingId: data.listingId,
       bookingId: bookingId,
+      listingId: data.listingId,
+      confirmationCode: confirmationCode,
       hostId: data.hostId,
       guestId: data.guestId,
-      reservations: data.reservations,
-      quantity: data.quantity,
+      quantity: 1,
       basePrice: data.basePrice,
-      fees: data.fees,
       currency: data.currency,
-      guestServiceFee: data.guestServiceFee,
-      hostServiceFee: data.hostServiceFee,
+      guestServiceFee: guestServiceFee,
+      hostServiceFee: hostServiceFee,
       totalPrice: data.totalPrice,
-      confirmationCode: data.confirmationCode,
-      paymentState: "pending",
-      payoutId: data.payoutId,
-      bookingState: "pending",
+      paymentState: 'pending',
+      bookingState: 'pending',
       bookingType: data.bookingType,
-      paymentMethodId: data.paymentMethodId,
-      subscriptionId: data.subscriptionId,
-      sourceId: data.sourceId,
       priceType: data.priceType,
+      periodQuantity: data.periodQuantity,
+      reservations: data.reservations,
       updatedAt: Date.now(),
       createdAt: Date.now()
     }
-  }
+  };
 
   const paramsQueue = {
     QueueUrl: queueUrl,
-    MessageBody: JSON.stringify({ bookingId: bookingId, listingId: data.listingId, blockedDates: data.reservations })
-  }
+    MessageBody: JSON.stringify({
+      bookingId: bookingId,
+      listingId: data.listingId,
+      blockedDates: data.reservations
+    })
+  };
 
   try {
-    await dynamoDbLib.call("put", params);
+    await dynamoDbLib.call('put', params);
     await queueLib.call(paramsQueue);
     return success(params.Item);
   } catch (e) {
-    console.log(e)
-    return failure({ status: false })
+    return failure({ status: 'error', error: e });
   }
-  
-}
+};
