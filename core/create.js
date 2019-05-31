db25516aa85aaf34e5a6dd8d5ed169d2302b4347
Arthemus <arthemus.moreira@gmail.com>
2019-05-31 15:27:41 +1000

    Update booking process.

--- core/create.js
+++ core/create.js
@@ -1,30 +1,47 @@
-import uuid from "uuid"
+import uuid from 'uuid';
 
-import * as dynamoDbLib from "../libs/dynamodb-lib"
-import * as queueLib from "../libs/queue-lib"
-import { success, failure } from "../libs/response-lib"
-import { calcTotal, getDates, getEndDate } from '../validations'
+import * as dynamoDbLib from '../libs/dynamodb-lib';
+import * as queueLib from '../libs/queue-lib';
+import { success, failure } from '../libs/response-lib';
+import { calcTotal, getDates, getEndDate } from '../validations';
+
+const IS_ABSORVE = 0.035;
+const NO_ABSORVE = 0.135;
 
 export const main = async (event, context) => {
-
-  const data = JSON.parse(event.body)
+  const data = JSON.parse(event.body);
   const queueUrl = `https://sqs.${process.env.region}.amazonaws.com/${process.env.accountId}/${process.env.queueName}`;
+
   const bookingId = uuid.v1();
 
   const confirmationCode = Math.floor((100000 + Math.random()) * 900000);
-  const guestServiceFee = data.isAbsorvedFee ? 1.035 : 1.135;
+  const guestServiceFee = data.isAbsorvedFee ? IS_ABSORVE : NO_ABSORVE;
   const hostServiceFee = data.isAbsorvedFee ? 1.1 : 1;
 
   let reservationDates;
   let totalPrice;
 
-  if (data.priceType === 'daily'){
+  if (data.priceType === 'daily') {
     reservationDates = data.reservations;
-    totalPrice = calcTotal(data.basePrice, data.quantity, reservationDates.length, guestServiceFee);
+    totalPrice = calcTotal(
+      data.basePrice,
+      data.quantity,
+      reservationDates.length,
+      guestServiceFee
+    );
   } else {
-    const endDate = getEndDate(data.reservations[0], data.period, data.priceType);
+    const endDate = getEndDate(
+      data.reservations[0],
+      data.period,
+      data.priceType
+    );
     reservationDates = getDates(data.reservations[0], endDate);
-    totalPrice = calcTotal(data.basePrice, data.quantity, data.period, guestServiceFee);
+    totalPrice = calcTotal(
+      data.basePrice,
+      data.quantity,
+      data.period,
+      guestServiceFee
+    );
   }
 
   const params = {
@@ -44,9 +61,9 @@ export const main = async (event, context) => {
       hostServiceFee: hostServiceFee,
       totalPrice: totalPrice,
       confirmationCode: confirmationCode,
-      paymentState: "pending",
+      paymentState: 'pending',
       payoutId: data.payoutId,
-      bookingState: "pending",
+      bookingState: 'pending',
       bookingType: data.bookingType,
       paymentMethodId: data.paymentMethodId,
       subscriptionId: data.subscriptionId,
@@ -55,20 +72,22 @@ export const main = async (event, context) => {
       updatedAt: Date.now(),
       createdAt: Date.now()
     }
-  }
+  };
 
   const paramsQueue = {
     QueueUrl: queueUrl,
-    MessageBody: JSON.stringify({ bookingId: bookingId, listingId: data.listingId, blockedDates: reservationDates })
-  }
+    MessageBody: JSON.stringify({
+      bookingId: bookingId,
+      listingId: data.listingId,
+      blockedDates: reservationDates
+    })
+  };
 
   try {
-    await dynamoDbLib.call("put", params);
+    await dynamoDbLib.call('put', params);
     await queueLib.call(paramsQueue);
     return success(params.Item);
   } catch (e) {
-    console.log(e)
-    return failure({ status: false })
+    return failure({ status: false, error: e });
   }
-  
-}
+};
--- validations/index.js
+++ validations/index.js
@@ -1,42 +1,36 @@
 import moment from 'moment';
 
 const calcTotal = (basePrice, quantity = 1, period, guestFee) => {
-  return basePrice * quantity * period * guestFee;
-}
+  let total = basePrice * quantity * period;
+  total += total * guestFee;
+  return total;
+};
 
 const getEndDate = (startDate, period, priceType) => {
-  
   var eDate;
-
-  switch(priceType) {
+  switch (priceType) {
     case 'daily':
       eDate = moment(startDate).add(period, 'days');
-    break;
+      break;
     case 'weekly':
       eDate = moment(startDate).add(period, 'weeks');
-    break;
+      break;
     case 'monthly':
       eDate = moment(startDate).add(period, 'months');
-    break;
+      break;
   }
-
   return eDate;
-
-}
+};
 
 const getDates = (startDate, endDate) => {
-
   var arrDates = [];
   var sDate = moment(startDate);
   var eDate = moment(endDate);
-  
   while (sDate < eDate) {
     arrDates.push(sDate.toISOString());
     sDate = sDate.clone().add(1, 'd');
   }
-
   return arrDates;
-
-}
+};
 
 export { calcTotal, getDates, getEndDate };
