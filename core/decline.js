929000bed3ff3df7c7930469e895f722d35195f4
Arthemus <arthemus.moreira@gmail.com>
2019-06-11 11:17:10 +1000

    Returning booking state after declined.

--- core/decline.js
+++ core/decline.js
@@ -1,30 +1,28 @@
-import * as dynamoDbLib from "../libs/dynamodb-lib"
-import { success, failure } from "../libs/response-lib"
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
 
 export async function main(event, context) {
-
   const params = {
     TableName: process.env.tableName,
     Key: {
       bookingId: event.pathParameters.id
     },
     ExpressionAttributeNames: {
-      '#booking_state': 'bookingState',
+      "#booking_state": "bookingState"
     },
     ExpressionAttributeValues: {
       ":bookingState": "declined",
       ":updatedAt": Date.now() || null
     },
-    UpdateExpression: "SET #booking_state = :bookingState, updatedAt = :updatedAt",
+    UpdateExpression:
+      "SET #booking_state = :bookingState, updatedAt = :updatedAt",
     ReturnValues: "ALL_NEW"
-  }
-
+  };
   try {
-    await dynamoDbLib.call("update", params)
-    return success({ status: true })
+    const { Attributes } = await dynamoDbLib.call("update", params);
+    return success({ status: true, data: Attributes });
   } catch (e) {
-    console.log(e)
-    return failure({ status: false })
+    console.log(e);
+    return failure({ status: false });
   }
-
 }
