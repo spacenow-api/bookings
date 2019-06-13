bf2925a281b7d610ef5a6c1338155e42599603f7
Arthemus <arthemus.moreira@gmail.com>
2019-06-13 14:37:21 +1000

    Add dotenv plugin and approach to use dynamodb local.

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,6 +1,16 @@
-import AWS from "aws-sdk";
+import AWS from 'aws-sdk';
+
+const checkEnvironment = () => {
+  if (process.env.DYNAMODB_LOCAL_SERVER) {
+    AWS.config.update({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    });
+  }
+};
 
 export const call = (action, params) => {
+  checkEnvironment();
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
   return dynamoDb[action](params).promise();
-}
+};
