ee7ef06d0801d5e2479eeb56746da16617cfd2eb
Camila <kmi_218@hotmail.com>
2019-06-18 14:53:30 +1000

    getByCompleted API

---
+++ core/getByCompleted.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from '../libs/dynamodb-lib';
+import { success, failure } from '../libs/response-lib';
+import moment from 'moment'
+
+export const main = async (event, context) => {
+  const currentDate = moment();
+  const params = {
+    TableName: process.env.tableName,
+    FilterExpression: `#bookingState = :bookingState && #checkOut < :currentDate`,
+    ExpressionAttributeNames: {
+      '#bookingState': 'bookingState',
+      '#checkOut': 'checkOut'
+    },
+    ExpressionAttributeValues: {
+      ':bookingState': 'approved',
+      ':currentDate': currentDate
+    }
+  };
+  try {
+    const result = await dynamoDbLib.call('scan', params);
+    return success({ count: result.Items.length, bookings: result.Items });
+  } catch (e) {
+    console.error(e);
+    return failure({ status: false });
+  }
+};
