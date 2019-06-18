fcff30286de72999edfc78b07ae090e8f379f2b2
Camila <kmi_218@hotmail.com>
2019-06-18 16:04:40 +1000

    getByCompleted API

--- core/getByCompleted.js
+++ core/getByCompleted.js
@@ -6,7 +6,7 @@ export const main = async (event, context) => {
   const currentDate = moment().toString();
   const params = {
     TableName: process.env.tableName,
-    FilterExpression: `#bookingState = :bookingState AND #checkOut > :currentDate`,
+    FilterExpression: `#bookingState = :bookingState AND` + moment(`#checkOut`)  + `> :currentDate`,
     ExpressionAttributeNames: {
       '#bookingState': 'bookingState',
       '#checkOut': 'checkOut'
