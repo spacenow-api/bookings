68ae72ea8cd523e3a66ce41f346c5c33edfb40c2
Camila <kmi_218@hotmail.com>
2019-06-19 14:51:52 +1000

    Fix completed API

--- core/getByCompleted.js
+++ core/getByCompleted.js
@@ -3,7 +3,7 @@ import { success, failure } from '../libs/response-lib';
 import moment from 'moment'
 
 export const main = async (event, context) => {
-  const nextDay = moment().add(1, 'days').format('YYYY-MM-DD').toString();
+  const nextDay = moment().subtract(1, 'days').format('YYYY-MM-DD').toString();
   const params = {
     TableName: process.env.tableName,
     FilterExpression: `#bookingState = :bookingState AND #checkOut = :nextDay`,
