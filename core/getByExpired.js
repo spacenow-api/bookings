a3cfd58a8bf65de420d2e0d3e70ae9e26546ee74
Camila <kmi_218@hotmail.com>
2019-06-19 14:34:22 +1000

    Expired API

--- core/getByExpired.js
+++ core/getByExpired.js
@@ -3,7 +3,7 @@ import { success, failure } from '../libs/response-lib';
 import moment from 'moment'
 
 export const main = async (event, context) => {
-  let plusTime = moment().add(1, 'days').unix();
+  let plusTime = moment().add(1, 'days').unix()*1000;
   // plusTime = new Date(plusTime)
   console.log('plusTime', plusTime)
   const params = {
