acd6867ca5837bae8756f44982a02b6f726fc840
Camila <kmi_218@hotmail.com>
2019-07-24 14:29:24 +1000

    Timeout API

---
+++ core/timeOut.js
@@ -0,0 +1,37 @@
+import * as dynamoDbLib from '../libs/dynamodb-lib';
+import { success, failure } from '../libs/response-lib';
+
+import { main as getBookings } from './get';
+
+export async function main(event, context) {
+
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      bookingId: event.pathParameters.id
+    },
+    ExpressionAttributeNames: {
+      '#booking_state': 'bookingState'
+    },
+    ExpressionAttributeValues: {
+      ':bookingState': 'timeout',
+      ':updatedAt': Date.now() || null
+    },
+    UpdateExpression:
+      'SET #booking_state = :bookingState, updatedAt = :updatedAt',
+    ReturnValues: 'ALL_NEW'
+  };
+
+  try {
+    setTimeout(function(){
+      const booking = await getBookings(event.pathParameters.id);
+      if (booking.bookingState == 'pending') {
+        await dynamoDbLib.call('update', params);
+      }
+      return success({ status: true });
+     }, 60000);
+  } catch (e) {
+    console.error(e);
+    return failure({ status: false });
+  }
+}
