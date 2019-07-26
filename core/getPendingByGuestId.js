eb0684dbbd6a6347c8145a01e59662068f3edbc2
Camila <kmi_218@hotmail.com>
2019-07-26 15:47:26 +1000

    timeout api

--- core/getPendingByGuestId.js
+++ core/getPendingByGuestId.js
@@ -2,6 +2,10 @@ import * as dynamoDbLib from '../libs/dynamodb-lib';
 import { success, failure } from '../libs/response-lib';
 
 export const main = async (event, context) => {
+  console.log('guestId', event.pathParameters.id)
+  console.log('typeof guestId', typeof event.pathParameters.id)
+  console.log('listingId', event.pathParameters.listingId)
+  console.log('typeof listingId', typeof event.pathParameters.listingId)
   const params = {
     TableName: process.env.tableName,
     Key: {
