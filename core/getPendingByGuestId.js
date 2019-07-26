b945cd3a2d5cfe6ae542c15ec384bdadddceeddf
Camila <kmi_218@hotmail.com>
2019-07-26 14:33:49 +1000

    Get pending booking by user API

---
+++ core/getPendingByGuestId.js
@@ -0,0 +1,22 @@
+import * as dynamoDbLib from '../libs/dynamodb-lib';
+import { success, failure } from '../libs/response-lib';
+
+export const main = async (event, context) => {
+  let expirationTime = Date.now() - 60000;  // 1 minute expire to test
+  const params = {
+    TableName: process.env.tableName,
+    FilterExpression: 'guestId = :guestId AND bookingState = :bookingState AND createdAt >= :expirationTime',
+    ExpressionAttributeValues: {
+      ':guestId': event.pathParameters.id,
+      ':bookingState': 'pending',
+      ':expiredTime': expirationTime
+    }
+  };
+  try {
+    const result = await dynamoDbLib.call('scan', params);
+    return success({ count: result.Items.length, bookings: result.Items });
+  } catch (e) {
+    console.error(e);
+    return failure({ status: false });
+  }
+};
