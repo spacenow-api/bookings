ce927cd73bf3a80b320c82fd3fd6219769afca2d
Arthemus <arthemus.moreira@gmail.com>
2019-08-02 09:46:05 +1000

    Check booking state.

--- core/approve.js
+++ core/approve.js
@@ -1,38 +1,38 @@
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { success, failure } from '../libs/response-lib';
+import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+import { BookingStates } from './../validations'
 
 export async function main(event) {
-  const bookingId = event.pathParameters.id;
-
-  // const { Item: bookingObj } = await dynamoDbLib.call('get', {
-  //   TableName: process.env.tableName,
-  //   Key: {
-  //     bookingId: bookingId
-  //   }
-  // });
-
-  // const bookingState =
-  //   bookingObj.bookingType === 'instant' ? 'approved' : 'requested';
-
-  const params = {
+  const bookingId = event.pathParameters.id
+  const { Item: bookingObj } = await dynamoDbLib.call('get', {
     TableName: process.env.tableName,
     Key: {
       bookingId: bookingId
-    },
-    ExpressionAttributeValues: {
-      ':updatedAt': Date.now(),
-      ':bookingState': 'approved',
-      ':paymentState': 'completed'
-    },
-    UpdateExpression:
-      'SET bookingState = :bookingState, paymentState = :paymentState, updatedAt = :updatedAt',
-    ReturnValues: 'ALL_NEW'
-  };
-
-  try {
-    const { Attributes } = await dynamoDbLib.call('update', params);
-    return success({ status: true, data: Attributes });
-  } catch (e) {
-    return failure({ status: 'error', error: e });
+    }
+  })
+  console.log('bookingObj', bookingObj)
+  if (BookingStates.REQUESTED === bookingObj.bookingState) {
+    // const params = {
+    //   TableName: process.env.tableName,
+    //   Key: {
+    //     bookingId: bookingId
+    //   },
+    //   ExpressionAttributeValues: {
+    //     ':updatedAt': Date.now(),
+    //     ':bookingState': 'approved',
+    //     ':paymentState': 'completed'
+    //   },
+    //   UpdateExpression:
+    //     'SET bookingState = :bookingState, paymentState = :paymentState, updatedAt = :updatedAt',
+    //   ReturnValues: 'ALL_NEW'
+    // }
+    // try {
+    //   const { Attributes } = await dynamoDbLib.call('update', params)
+    //   return success({ status: true, data: Attributes })
+    // } catch (e) {
+    //   return failure({ status: 'error', error: e })
+    // }
+  } else {
+    console.warn(`Booking ${bookingId} is not Requested.`)
   }
 }
