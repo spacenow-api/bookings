46fdc8291b07a8adf949ab6f43b6efd7a0208b98
Arthemus <arthemus.moreira@gmail.com>
2019-09-26 14:06:37 +1000

    Get by expired with mysql.

--- core/getByExpired.js
+++ core/getByExpired.js
@@ -1,29 +1,46 @@
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { success, failure } from '../libs/response-lib';
 import moment from 'moment'
+import { Op } from 'sequelize'
 
-export const main = async (event, context) => {
-  let lessHour = moment().subtract(24, 'hours').subtract(30, 'minutes').unix() * 1000;
-  let plusHour = moment().subtract(24, 'hours').add(30, 'minutes').unix() * 1000;
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+import { BookingStates } from './../validations'
+import { Bookings } from './../models'
 
-  const params = {
-    TableName: process.env.tableName,
-    FilterExpression: `#bookingState = :bookingState AND #createdAt BETWEEN :lessHour AND :plusHour`,
-    ExpressionAttributeNames: {
-      '#bookingState': 'bookingState',
-      '#createdAt': 'createdAt'
-    },
-    ExpressionAttributeValues: {
-      ':bookingState': 'requested',
-      ':plusHour': plusHour,
-      ':lessHour': lessHour
-    }
-  };
+export const main = async () => {
   try {
-    const result = await dynamoDbLib.call('scan', params);
-    return success({ count: result.Items.length, items: result.Items });
-  } catch (e) {
-    console.error(e);
-    return failure({ status: false });
+    const lessHour =
+      moment()
+        .subtract(24, 'hours')
+        .subtract(30, 'minutes')
+        .unix() * 1000
+    const plusHour =
+      moment()
+        .subtract(24, 'hours')
+        .add(30, 'minutes')
+        .unix() * 1000
+    // const result = await dynamoDbLib.call('scan', {
+    //   TableName: process.env.tableName,
+    //   FilterExpression: `#bookingState = :bookingState AND #createdAt BETWEEN :lessHour AND :plusHour`,
+    //   ExpressionAttributeNames: {
+    //     '#bookingState': 'bookingState',
+    //     '#createdAt': 'createdAt'
+    //   },
+    //   ExpressionAttributeValues: {
+    //     ':bookingState': 'requested',
+    //     ':plusHour': plusHour,
+    //     ':lessHour': lessHour
+    //   }
+    // })
+    // return success({ count: result.Items.length, items: result.Items })
+    const bookings = await Bookings.findAll({
+      where: {
+        bookingState: BookingStates.REQUESTED,
+        createdAt: { [Op.between]: [lessHour, plusHour] }
+      }
+    })
+    return success({ count: bookings.length, items: bookings })
+  } catch (err) {
+    console.error(err)
+    return failure({ status: false, error: err })
   }
-};
+}
