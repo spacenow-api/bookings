0c99518a2d327e2f5d132c5ffc92e780ab127436
Arthemus <arthemus.moreira@gmail.com>
2019-09-26 14:42:17 +1000

    Decline bookings with mysql.

--- core/decline.js
+++ core/decline.js
@@ -1,44 +1,47 @@
 import AWS from 'aws-sdk'
 
-import * as dynamoDbLib from '../libs/dynamodb-lib'
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
 import { success, failure } from '../libs/response-lib'
 import { BookingStates } from './../validations'
+import { onCleanAvailabilities } from './../helpers/availabilities.function'
+import updateBookingState from './../helpers/updateBookingState'
+import { Bookings } from './../models'
 
 const lambda = new AWS.Lambda()
 
 export async function main(event) {
   const bookingId = event.pathParameters.id
-  const { Item: bookingObj } = await dynamoDbLib.call('get', {
-    TableName: process.env.tableName,
-    Key: {
-      bookingId: bookingId
-    }
-  })
+  // const { Item: bookingObj } = await dynamoDbLib.call('get', {
+  //   TableName: process.env.tableName,
+  //   Key: {
+  //     bookingId: bookingId
+  //   }
+  // })
+  const bookingObj = await Bookings.findOne({ where: { bookingId } })
   if (BookingStates.REQUESTED === bookingObj.bookingState) {
-    const params = {
-      TableName: process.env.tableName,
-      Key: {
-        bookingId: event.pathParameters.id
-      },
-      ExpressionAttributeNames: {
-        '#booking_state': 'bookingState'
-      },
-      ExpressionAttributeValues: {
-        ':bookingState': BookingStates.DECLINED,
-        ':updatedAt': Date.now()
-      },
-      UpdateExpression:
-        'SET #booking_state = :bookingState, updatedAt = :updatedAt',
-      ReturnValues: 'ALL_NEW'
-    }
     try {
-      const { Attributes } = await dynamoDbLib.call('update', params)
+      // const { Attributes } = await dynamoDbLib.call('update', {
+      //   TableName: process.env.tableName,
+      //   Key: {
+      //     bookingId: bookingId
+      //   },
+      //   ExpressionAttributeNames: {
+      //     '#booking_state': 'bookingState'
+      //   },
+      //   ExpressionAttributeValues: {
+      //     ':bookingState': BookingStates.DECLINED,
+      //     ':updatedAt': Date.now()
+      //   },
+      //   UpdateExpression: 'SET #booking_state = :bookingState, updatedAt = :updatedAt',
+      //   ReturnValues: 'ALL_NEW'
+      // })
+      const bookingUpdated = await updateBookingState(bookingId, BookingStates.DECLINED)
       await onCleanAvailabilities(bookingId)
       await onSendDeclinedEmail(bookingId)
-      return success({ status: true, data: Attributes })
-    } catch (e) {
-      console.error(e)
-      return failure({ status: false })
+      return success({ status: true, data: bookingUpdated })
+    } catch (err) {
+      console.error(err)
+      return failure({ status: false, error: err })
     }
   } else {
     console.warn(`Booking ${bookingId} is not Requested.`)
@@ -46,28 +49,11 @@ export async function main(event) {
   }
 }
 
-const onCleanAvailabilities = async (bookingId) => {
-  const environment = process.env.environment
-  return await lambda.invoke(
-    {
-      FunctionName: `spacenow-availabilities-api-${environment}-deleteByBooking`,
-      Payload: JSON.stringify({ pathParameters: { id: bookingId } })
-    },
-    (error) => {
-      if (error) {
-        throw new Error(error)
-      }
-      console.info(`Availabilities removed with success to booking ${bookingId}`)
-    }
-  ).promise()
-}
-
 const onSendDeclinedEmail = (bookingId) => {
   return new Promise((resolve, reject) => {
-    const environment = process.env.environment
     lambda.invoke(
       {
-        FunctionName: `api-emails-${environment}-sendEmailByBookingDeclined`,
+        FunctionName: `api-emails-${process.env.environment}-sendEmailByBookingDeclined`,
         Payload: JSON.stringify({ pathParameters: { bookingId: bookingId } })
       },
       (error) => {
--- core/getByHostId.js
+++ core/getByHostId.js
@@ -1,6 +1,6 @@
 import { Op } from 'sequelize'
 
-import * as dynamoDbLib from '../libs/dynamodb-lib'
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
 import { success, failure } from '../libs/response-lib'
 import { BookingStates, mapReservations } from './../validations'
 import { Bookings } from './../models'
