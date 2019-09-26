7fbb1dcb5390874c85ac7adf9edd3738f4ce2307
Arthemus <arthemus.moreira@gmail.com>
2019-09-26 12:40:56 +1000

    Timeout booking on mysql.

--- core/timeout.js
+++ core/timeout.js
@@ -1,55 +1,38 @@
-import AWS from 'aws-sdk'
+// import AWS from 'aws-sdk'
 
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { success, failure } from '../libs/response-lib';
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+import updateBookingState from './../helpers/updateBookingState'
+import { onCleanAvailabilities } from './../helpers/availabilities.function'
+import { BookingStates } from './../validations'
+// import { Bookings } from './../models'
 
-import { BookingStates } from './../validations';
-
-const lambda = new AWS.Lambda()
+// const lambda = new AWS.Lambda()
 
 export async function main(event, context) {
-  const params = {
-    TableName: process.env.tableName,
-    Key: {
-      bookingId: event.pathParameters.id
-    },
-    ExpressionAttributeNames: {
-      '#booking_state': 'bookingState'
-    },
-    ExpressionAttributeValues: {
-      ':bookingState': BookingStates.TIMEOUT,
-      ':updatedAt': Date.now() || null
-    },
-    UpdateExpression:
-      'SET #booking_state = :bookingState, updatedAt = :updatedAt',
-    ReturnValues: 'ALL_NEW'
-  };
   try {
-    const { Attributes } = await dynamoDbLib.call('update', params);
+    // const { Attributes } = await dynamoDbLib.call('update', {
+    //   TableName: process.env.tableName,
+    //   Key: {
+    //     bookingId: event.pathParameters.id
+    //   },
+    //   ExpressionAttributeNames: {
+    //     '#booking_state': 'bookingState'
+    //   },
+    //   ExpressionAttributeValues: {
+    //     ':bookingState': BookingStates.TIMEOUT,
+    //     ':updatedAt': Date.now() || null
+    //   },
+    //   UpdateExpression: 'SET #booking_state = :bookingState, updatedAt = :updatedAt',
+    //   ReturnValues: 'ALL_NEW'
+    // })
+    // await onCleanAvailabilities(event.pathParameters.id)
+    // return success({ status: true, data: Attributes })
+    const bookingUpdated = await updateBookingState(event.pathParameters.id, BookingStates.TIMEOUT)
     await onCleanAvailabilities(event.pathParameters.id)
-    return success({ status: true,  data: Attributes });
-  } catch (e) {
-    console.error(e);
-    return failure({ status: false });
+    return success({ status: true, data: bookingUpdated })
+  } catch (err) {
+    console.error(err)
+    return failure({ status: false, error: err })
   }
-}
-
-const onCleanAvailabilities = async bookingId => {
-  const environment = process.env.environment;
-  await lambda
-    .invoke(
-      {
-        FunctionName: `spacenow-availabilities-api-${environment}-deleteByBooking`,
-        Payload: JSON.stringify({ pathParameters: { id: bookingId } })
-      },
-      error => {
-        if (error) {
-          throw new Error(error)
-        }
-        console.info(
-          `Availabilities removed with success to booking ${bookingId}`
-        )
-      }
-    )
-    .promise()
 }
