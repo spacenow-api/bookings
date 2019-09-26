70fd0dc5a0f617e26205e57b51733908cbc530fc
Arthemus <arthemus.moreira@gmail.com>
2019-09-26 13:59:59 +1000

    Get bookings completed with mysql.

--- core/getByCompleted.js
+++ core/getByCompleted.js
@@ -1,26 +1,38 @@
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { success, failure } from '../libs/response-lib';
 import moment from 'moment'
 
-export const main = async (event, context) => {
-  const nextDay = moment().subtract(1, 'days').format('YYYY-MM-DD').toString();
-  const params = {
-    TableName: process.env.tableName,
-    FilterExpression: `#bookingState = :bookingState AND #checkOut = :nextDay`,
-    ExpressionAttributeNames: {
-      '#bookingState': 'bookingState',
-      '#checkOut': 'checkOut'
-    },
-    ExpressionAttributeValues: {
-      ':bookingState': 'approved',
-      ':nextDay': nextDay
-    }
-  };
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+import { BookingStates } from './../validations'
+import { Bookings } from './../models'
+
+export const main = async () => {
   try {
-    const result = await dynamoDbLib.call('scan', params);
-    return success({ count: result.Items.length, items: result.Items });
-  } catch (e) {
-    console.error(e);
-    return failure({ status: false });
+    // const result = await dynamoDbLib.call('scan', {
+    //   TableName: process.env.tableName,
+    //   FilterExpression: `#bookingState = :bookingState AND #checkOut = :nextDay`,
+    //   ExpressionAttributeNames: {
+    //     '#bookingState': 'bookingState',
+    //     '#checkOut': 'checkOut'
+    //   },
+    //   ExpressionAttributeValues: {
+    //     ':bookingState': 'approved',
+    //     ':nextDay': nextDay
+    //   }
+    // })
+    // return success({ count: result.Items.length, items: result.Items })
+    const nextDay = moment()
+      .subtract(1, 'days')
+      .format('YYYY-MM-DD')
+      .toString()
+    const bookings = await Bookings.findAll({
+      where: {
+        bookingState: BookingStates.APPROVED,
+        checkOut: nextDay
+      }
+    })
+    return success({ count: bookings.length, items: bookings })
+  } catch (err) {
+    console.error(err)
+    return failure({ status: false, error: err })
   }
-};
+}
