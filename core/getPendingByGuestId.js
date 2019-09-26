2bbbb5a74272557301fed9d3d6a24f30d7df4224
Arthemus <arthemus.moreira@gmail.com>
2019-09-26 14:23:51 +1000

    Getting pending by guest and listing.

--- core/getPendingByGuestId.js
+++ core/getPendingByGuestId.js
@@ -1,21 +1,30 @@
-import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { success, failure } from '../libs/response-lib';
+// import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+import { BookingStates, mapReservations } from './../validations'
+import { Bookings } from './../models'
 
-export const main = async (event, context) => {
-  const params = {
-    TableName: process.env.tableName,
-    FilterExpression: 'listingId = :listingId AND guestId = :guestId AND bookingState = :bookingState',
-    ExpressionAttributeValues: {
-      ':guestId': event.pathParameters.id,
-      ':listingId': parseInt(event.pathParameters.listingId),
-      ':bookingState': 'pending'
-    }
-  };
+export const main = async (event) => {
   try {
-    const result = await dynamoDbLib.call('scan', params);
-    return success({ count: result.Items.length, items: result.Items });
-  } catch (e) {
-    console.error(e);
-    return failure({ status: false });
+    // const result = await dynamoDbLib.call('scan', {
+    //   TableName: process.env.tableName,
+    //   FilterExpression: 'listingId = :listingId AND guestId = :guestId AND bookingState = :bookingState',
+    //   ExpressionAttributeValues: {
+    //     ':guestId': event.pathParameters.id,
+    //     ':listingId': parseInt(event.pathParameters.listingId),
+    //     ':bookingState': 'pending'
+    //   }
+    // })
+    // return success({ count: result.Items.length, items: result.Items })
+    const bookings = await Bookings.findAll({
+      where: {
+        guestId: event.pathParameters.id,
+        listingId: parseInt(event.pathParameters.listingId),
+        bookingState: BookingStates.PENDING
+      }
+    })
+    return success({ count: bookings.length, items: bookings.map(mapReservations) })
+  } catch (err) {
+    console.error(err)
+    return failure({ status: false, error: err })
   }
-};
+}
