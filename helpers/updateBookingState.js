dafe71c75b7e9a02f4ac8a78e7a3f62d22b4ff61
Arthemus <arthemus.moreira@gmail.com>
2019-09-30 09:30:55 +1000

    Cleaning code comments and removing unused files.

--- helpers/updateBookingState.js
+++ helpers/updateBookingState.js
@@ -1,22 +1,8 @@
-// import * as dynamoDbLib from '../libs/dynamodb-lib'
 import { mapReservations } from './../validations'
 import { Bookings } from './../models'
 
 export default async (bookingId, state) => {
   try {
-    // const { Attributes } = await dynamoDbLib.call('update', {
-    //   TableName: process.env.tableName,
-    //   Key: {
-    //     bookingId: bookingId
-    //   },
-    //   ExpressionAttributeValues: {
-    //     ':bookingState': state,
-    //     ':updatedAt': Date.now()
-    //   },
-    //   UpdateExpression: 'SET bookingState = :bookingState, updatedAt = :updatedAt',
-    //   ReturnValues: 'ALL_NEW'
-    // })
-    // return Attributes
     await Bookings.update({ bookingState: state }, { where: { bookingId } })
     const bookingObj = await Bookings.findOne({ where: { bookingId } })
     return mapReservations(bookingObj)
