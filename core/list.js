66fa2b6f8ad65660ca98ff46bb36c79dc16febf0
Arthemus <arthemus.moreira@gmail.com>
2019-10-01 12:28:12 +1000

    Testing serverless configuration.

--- core/list.js
+++ core/list.js
@@ -3,8 +3,10 @@ import { Bookings } from './../models'
 import { resolveBooking } from './../validations'
 
 export const main = async () => {
+  console.log('Main list function running...')
   try {
     const result = await Bookings.findAll({ raw: true })
+    console.log('Bookings found: ', result)
     result.map(resolveBooking)
     return success(result)
   } catch (err) {
