168b1bb077d245d8e237c7ca1d716ce084393715
Arthemus <arthemus.moreira@gmail.com>
2019-10-08 13:05:35 +1100

    Creating new tests.

--- test/hourly.test.js
+++ test/hourly.test.js
@@ -12,6 +12,10 @@ test('Expecting 4 hours as a valid period.', () => {
   expect(getHourlyPeriod('12:00:00', '16:00:00')).toBe(4)
 })
 
+test('Expecting 5 hours even with minutes included.', () => {
+  expect(getHourlyPeriod('12:30:00', '17:30:00')).toBe(5)
+})
+
 test('Expecting an exception error when receive a half hour.', () => {
   expect(() => getHourlyPeriod('12:30:00', '16:00:00')).toThrow('It is not possible to book a space with a half or less minutes of diference.')
 })
