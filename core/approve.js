ea744a73055f7078facb9752cea11c73e363598d
Arthemus <arthemus.moreira@gmail.com>
2019-10-17 16:12:41 +1100

    Clean code commented.

--- core/approve.js
+++ core/approve.js
@@ -1,11 +1,7 @@
-// import AWS from 'aws-sdk'
-
 import { success, failure } from '../libs/response-lib'
 import { onSendEmail } from './../helpers/email.function'
 import { BookingStates, resolveBooking } from './../validations'
 import { Bookings } from './../models'
-
-// const lambda = new AWS.Lambda()
 
 export async function main(event) {
   const bookingId = event.pathParameters.id
@@ -28,21 +24,3 @@ export async function main(event) {
     return success({ status: true, data: resolveBooking(bookingObj), blockEmails: true })
   }
 }
-
-// const onSendEmail = (emailFunctionName, bookingId) => {
-//   return new Promise((resolve, reject) => {
-//     lambda.invoke(
-//       { 
-//         FunctionName: emailFunctionName,
-//         Payload: JSON.stringify({ pathParameters: { bookingId: bookingId } })
-//       }, (error) => {
-//         if (error) {
-//           reject(error)
-//         } else {
-//           console.info(`Approved email sent with success by booking ${bookingId}`)
-//           resolve()
-//         }
-//       }
-//     )
-//   })
-// }
--- core/request.js
+++ core/request.js
@@ -1,11 +1,7 @@
-// import AWS from 'aws-sdk'
-
 import { success, failure } from '../libs/response-lib'
 import { onSendEmail } from './../helpers/email.function'
 import { BookingStates, resolveBooking } from './../validations'
 import { Bookings } from './../models'
-
-// const lambda = new AWS.Lambda()
 
 export async function main(event) {
   try {
@@ -23,22 +19,3 @@ export async function main(event) {
     return failure({ status: false, error: err })
   }
 }
-
-// const onSendEmail = (emailFunctionName, bookingId) => {
-//   return new Promise((resolve, reject) => {
-//     lambda.invoke(
-//       {
-//         FunctionName: emailFunctionName,
-//         Payload: JSON.stringify({ pathParameters: { bookingId: bookingId } })
-//       },
-//       (error) => {
-//         if (error) {
-//           reject(error)
-//         } else {
-//           console.info(`Requested email sent with success by booking ${bookingId}`)
-//           resolve()
-//         }
-//       }
-//     )
-//   })
-// }
