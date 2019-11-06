ed8a5f6c5496a09efff872526a9e8dfbb50a6877
Camila <kmi_218@hotmail.com>
2019-11-06 11:11:17 +1100

    expired bookings cronjob

--- core/getByExpired.js
+++ core/getByExpired.js
@@ -4,6 +4,10 @@ import { Op } from 'sequelize'
 import { success, failure } from '../libs/response-lib'
 import { BookingStates, resolveBooking } from './../validations'
 import { Bookings } from './../models'
+import { onSendEmail } from './../helpers/email.function'
+
+import updateBookingState from './../helpers/updateBookingState'
+import { onCleanAvailabilities } from './../helpers/availabilities.function'
 
 export const main = async () => {
   try {
@@ -21,8 +25,16 @@ export const main = async () => {
       where: {
         bookingState: BookingStates.REQUESTED,
         createdAt: { [Op.between]: [lessHour, plusHour] }
-      }, raw: true
+      },
+      raw: true
     })
+    for (const item of bookings) {
+      await updateBookingState(item.bookingId, BookingStates.EXPIRED)
+      await onCleanAvailabilities(item.bookingId)
+      await onSendEmail(`api-emails-${process.env.environment}-sendEmailExpiredBooking`, item.bookingId)
+    }
+    console.log('bookings', bookings)
+    console.log('bookings', bookings.map(resolveBooking))
     return success({ count: bookings.length, items: bookings.map(resolveBooking) })
   } catch (err) {
     console.error(err)
