84f7eb1daba6dbf0dd26c76315a44774d9c2b02b
Lucas Nascimento <csalucasnascimento@gmail.com>
2019-11-12 15:48:31 +1100

    admin

--- core/count.js
+++ core/count.js
@@ -5,15 +5,21 @@ import { Op } from 'sequelize'
 
 export const main = async (event) => {
   const days = event.queryStringParameters.days
-  const date = subDays(new Date(), days);
-  try {
-    const data = await Bookings.count({
+  let where;
+  if (days) {
+    const date = subDays(new Date(), days);
+    where = {
       where: { 
         createdAt: { 
           [Op.gte]: `${date.getTime()}`
         }
       }
-    });
+    }
+  } else {
+    where = {}
+  }
+  try {
+    const data = await Bookings.count(where);
     return success({ count: data })
   } catch (error) {
     return failure({ status: false })
