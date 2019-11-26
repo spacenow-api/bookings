5b1dfab1ef455575d05c9db7d339151c97fe42c8
Arthemus <arthemus.moreira@gmail.com>
2019-11-26 12:31:52 +1100

    Refactoring decline function.

--- core/request.js
+++ core/request.js
@@ -3,8 +3,8 @@ import { success, failure } from '../libs/response-lib'
 import * as bookingService from './../services/booking.service'
 
 export async function main(event) {
+  const bookingId = event.pathParameters.id
   try {
-    const bookingId = event.pathParameters.id
     const bookingObjUpdated = await bookingService.doRequestedBooking(bookingId)
     return success({ status: true, data: bookingObjUpdated })
   } catch (err) {
--- services/booking.service.js
+++ services/booking.service.js
@@ -1,14 +1,23 @@
 import { onSendEmail } from './../helpers/email.function'
+import { onCleanAvailabilities } from './../helpers/availabilities.function'
+
 import { BookingStates, resolveBooking } from './../validations'
+
 import { Bookings } from './../models'
+
+async function doUpdateBookingState(bookingId, state) {
+  try {
+    await Bookings.update({ bookingState: state, updatedAt: Date.now() }, { where: { bookingId } })
+    const bookingObj = await Bookings.findOne({ where: { bookingId }, raw: true })
+    return bookingObj
+  } catch (err) {
+    throw err
+  }
+}
 
 async function doRequestedBooking(bookingId) {
   try {
-    await Bookings.update(
-      { bookingState: BookingStates.REQUESTED, paymentState: BookingStates.COMPLETED, updatedAt: Date.now() },
-      { where: { bookingId } }
-    )
-    const bookingObjUpdated = await Bookings.findOne({ where: { bookingId }, raw: true })
+    const bookingObjUpdated = await doUpdateBookingState(bookingId, BookingStates.REQUESTED)
     await onSendEmail(`api-emails-${process.env.environment}-sendEmailByBookingRequestHost`, bookingId)
     await onSendEmail(`api-emails-${process.env.environment}-sendEmailByBookingRequestGuest`, bookingId)
     return resolveBooking(bookingObjUpdated)
@@ -19,25 +28,39 @@ async function doRequestedBooking(bookingId) {
 }
 
 async function doApproveBooking(bookingId) {
-  const bookingObj = await Bookings.findOne({ where: { bookingId }, raw: true })
-  if (BookingStates.REQUESTED === bookingObj.bookingState || BookingStates.PENDING === bookingObj.bookingState) {
-    try {
-      await Bookings.update(
-        { bookingState: BookingStates.APPROVED, updatedAt: Date.now() },
-        { where: { bookingId } }
-      )
+  try {
+    const bookingObj = await Bookings.findOne({ where: { bookingId }, raw: true })
+    if (BookingStates.REQUESTED === bookingObj.bookingState || BookingStates.PENDING === bookingObj.bookingState) {
+      const bookingObjUpdated = await doUpdateBookingState(bookingId, BookingStates.APPROVED)
       await onSendEmail(`api-emails-${process.env.environment}-sendEmailByBookingInstantHost`, bookingId)
       await onSendEmail(`api-emails-${process.env.environment}-sendEmailByBookingInstantGuest`, bookingId)
-      const bookingObjUpdated = await Bookings.findOne({ where: { bookingId }, raw: true })
       return resolveBooking(bookingObjUpdated)
-    } catch (err) {
-      console.error(`Problems to update booking ${bookingId} to Approved:`, err)
-      throw err
+    } else {
+      console.warn(`Booking ${bookingId} is not Requested.`)
+      return resolveBooking(bookingObj)
     }
-  } else {
-    console.warn(`Booking ${bookingId} is not Requested.`)
-    return resolveBooking(bookingObj)
+  } catch (err) {
+    console.error(`Problems to update booking ${bookingId} to Approved:`, err)
+    throw err
   }
 }
 
-export { doRequestedBooking, doApproveBooking }
+async function doDeclineBooking(bookingId) {
+  try {
+    const bookingObj = await Bookings.findOne({ where: { bookingId } })
+    if (BookingStates.REQUESTED === bookingObj.bookingState) {
+      const bookingObjUpdated = await doUpdateBookingState(bookingId, BookingStates.DECLINED)
+      await onCleanAvailabilities(bookingId)
+      await onSendEmail(`api-emails-${process.env.environment}-sendEmailByBookingDeclined`, bookingId)
+      return resolveBooking(bookingObjUpdated)
+    } else {
+      console.warn(`Booking ${bookingId} is not Requested.`)
+      return resolveBooking(bookingObj)
+    }
+  } catch (err) {
+    console.error(`Problems to decline booking ${bookingId}:`, err)
+    throw err
+  }
+}
+
+export { doRequestedBooking, doApproveBooking, doDeclineBooking }
