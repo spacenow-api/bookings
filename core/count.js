11068d84ee5191d5d30e33d37e045a9e6a968381
Lucas <csalucasnascimento@gmail.com>
2020-01-23 13:52:47 +1100

    Fixing days undefined

--- core/count.js
+++ core/count.js
@@ -4,13 +4,17 @@ const { Bookings } = require('./../models')
 const { Op } = require('sequelize')
 
 module.exports.main = async (event, context, callback) => {
-  const days = event.queryStringParameters.days || 10000
+
+  let days = 10000;
+  if (event.queryStringParameters)
+    days = event.queryStringParameters.days
+
   let where;
   if (days) {
     const date = subDays(new Date(), days);
     where = {
-      where: { 
-        createdAt: { 
+      where: {
+        createdAt: {
           [Op.gte]: `${date.getTime()}`
         }
       }
@@ -25,7 +29,7 @@ module.exports.main = async (event, context, callback) => {
       where: { ...where, bookingState: "completed" }
     });
     const cancelled = await Bookings.count({
-      where: { ...where, bookingState: "cancelled",  }
+      where: { ...where, bookingState: "cancelled", }
     });
     return success({ count: { all, approved, completed, cancelled } })
   } catch (error) {
