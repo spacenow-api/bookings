adac1cb4b488a805c0daef50f1a16ed05203e874
Camila <kmi_218@hotmail.com>
2020-01-24 11:38:01 +1100

    send email when time out booking

--- core/timeout.js
+++ core/timeout.js
@@ -1,12 +1,14 @@
-const { success, failure } = require('../libs/response-lib');
+const { success, failure } = require('../libs/response-lib')
 const updateBookingState = require('./../helpers/updateBookingState')
 const { onCleanAvailabilities } = require('./../helpers/availabilities.function')
 const { BookingStates } = require('./../validations')
+const { onSendEmail } = require('./../helpers/email.function')
 
 module.exports.main = async (event, context, callback) => {
   try {
     const bookingUpdated = await updateBookingState(event.pathParameters.id, BookingStates.TIMEOUT)
     await onCleanAvailabilities(event.pathParameters.id)
+    await onSendEmail(`api-emails-${process.env.environment}-sendEmailBookingTimedOutGuest`, event.pathParameters.id)
     return success({ status: true, data: bookingUpdated })
   } catch (err) {
     console.error(err)
